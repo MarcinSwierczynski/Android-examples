e54b3d50b9d627ed4d3b20e032d17c221a41b77c
Marcin Swierczynski <marcin@swierczynski.net>
2011-03-26 11:24:10 +0100

    Hello World Activity

---
+++ src/net/swierczynski/android_examples/Hello.java
@@ -0,0 +1,11 @@
+package net.swierczynski.android_examples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Hello extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
